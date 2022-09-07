16346c0c85aeef6549beb281a1ab938046bf1447
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-09-08 00:13:32 +0900

    init

---
+++ src/index.test.ts
@@ -0,0 +1,54 @@
+import { Match, pathMatcher } from '.';
+
+it('basic', () => {
+  type RouteMatch = Match & {
+    tag: string;
+  };
+
+  const matches: RouteMatch[] = [
+    {
+      pattern: '',
+      tag: 'index',
+    },
+    {
+      pattern: 'about',
+      tag: 'about',
+    },
+    {
+      pattern: /^[0-9A-Z]{5}$/,
+      tag: 'post',
+      name: 'postID',
+      nexts: [
+        {
+          pattern: /^[0-9]{4}$/,
+          tag: 'comment',
+          name: 'commentID',
+        },
+      ],
+    },
+  ];
+
+  expect(pathMatcher(matches, '/')).toEqual({ match: matches[0], params: {} });
+  expect(pathMatcher(matches, '/about')).toEqual({
+    match: matches[1],
+    params: {},
+  });
+  expect(pathMatcher(matches, '/ABC12')).toEqual({
+    match: matches[2],
+    params: {
+      postID: 'ABC12',
+    },
+  });
+  expect(pathMatcher(matches, '/ABC12/0001')).toEqual({
+    match: matches[2]?.nexts?.[0],
+    params: {
+      postID: 'ABC12',
+      commentID: '0001',
+    },
+  });
+
+  expect(pathMatcher(matches, '')).toBeUndefined();
+  expect(pathMatcher(matches, '/notfound')).toBeUndefined();
+  expect(pathMatcher(matches, '/abc12')).toBeUndefined();
+  expect(pathMatcher(matches, '/ABC12/A001')).toBeUndefined();
+});
